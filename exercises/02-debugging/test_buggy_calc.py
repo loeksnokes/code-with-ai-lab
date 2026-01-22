2de04d58e20d09ff94b5e0599bcbfc1881d3fecb
loeksnokes <loeksnokes@gmail.com>
2026-01-22 07:39:42 +0000

    Fix buggy_calc and update tests for exercise 02

--- exercises/02-debugging/test_buggy_calc.py
+++ exercises/02-debugging/test_buggy_calc.py
@@ -1,10 +1,10 @@
 from buggy_calc import average
 
 def test_average_basic():
-    assert average([1, 2, 3]) == 2.0
+    assert average([1, 2, 4]) == 2.3333333333333335
 
 def test_average_floats():
-    assert average([1.0, 2.0, 3.0]) == 2.0
+    assert average([1.0, 2.0, 4.0]) == 2.3333333333333335
 
 def test_average_empty():
     assert average([]) == 0
